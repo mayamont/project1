69b1d3667b5cc75a0dfe6017b17ba01f41d1431c
mayamont <145682897+mayamont@users.noreply.github.com>
2023-11-05 00:46:36 -0400

    Add files via upload

--- project/MainScript.js
+++ project/MainScript.js
@@ -1,19 +1,8 @@
 // Creating de variables:
-var firstName;
-var lastName;
-var namevariable1;
-var namevariable2;
-var namevariable3;
-var retirees;
-var notretirees;
-let age;
-let nextAge;
-let fullName;
-const thisyear = 2023;
-const future = 2060;
-const firstname = document.getElementById("firstname");
-const lastname = document.getElementById("lastname");
-const actualage = document.getElementById("age");
+
+const YearsQualified = document.getElementById("yearqualified");
+const YearToday = document.getElementById("yeartoday");
+const age = document.getElementById("age");
 // Creates variables with the texts Ids to later change the texts according to the button clicked
 //about it
 const myElement = document.getElementById("texth1");
@@ -38,24 +27,6 @@ const myElement15 = document.getElementById("textpresult");
 let hidden3 = myElement15.getAttribute("hidden");
 
 
-// Assigning values to the variables:
-//age = 25;
-retirees = 0;
-notretirees = 0;
-//firstName = "Mayara";
-//lastName = "Monteiro da Silva";
-namevariable1 = "First name:";
-namevariable2 = "Last name:";
-namevariable3 = "Full name:";
-namevariable4 = "This year:";
-numberretirees = "People retiring: ";
-numbernotretirees = "People not retiring: ";
-
-
-// Function to write messages using the variables
-function displayvariable(a, b){
-    console.log(a + " " + b)
-}
 
 // Changes the texts when the About it button is clicked
 function Aboutit(a){
@@ -64,6 +35,7 @@ function Aboutit(a){
     var texto3 = "Text from: <a href='https://www.canada.ca/en/services/benefits/publicpensions/cpp.html'>Canadian government website</a>"
     myElement.innerHTML = "About the plan";
     myElement3.removeAttribute("hidden");
+    myElement2.removeAttribute("hidden");
     myElement2.innerHTML = texto;
     myElement4.innerHTML = texto2;
     myElement5.innerHTML = texto3;
@@ -78,34 +50,21 @@ function Aboutit(a){
 
 // Checking if the person is qualified for retirement and printing the message on the web page with the result
 function Result(a){
-    if(nextAge >= 60){
 
-        myElement11.innerHTML = "Your Result";
-        myElement13.innerHTML = "";
-        myElement14.innerHTML = "";
-        myElement3.setAttribute("hidden", "hidden");
-        myElement10.setAttribute("hidden", "hidden");
-        myElement15.removeAttribute("hidden");
-        retirees++;
-        displayvariable(numberretirees, retirees);
+    myElement11.innerHTML = "Summary";
+    myElement13.innerHTML = "";
+    myElement14.innerHTML = "";
+    myElement2.setAttribute("hidden", "hidden");
+    myElement3.setAttribute("hidden", "hidden");
+    myElement10.setAttribute("hidden", "hidden");
+    myElement15.removeAttribute("hidden");
+ 
 
-    
-    }
-    else{
-        myElement11.innerHTML = "Your Result";
-        myElement13.innerHTML = "";
-        myElement14.innerHTML = "";
-        myElement3.setAttribute("hidden", "hidden");
-        myElement10.setAttribute("hidden", "hidden");
-        myElement15.removeAttribute("hidden");
-        notretirees++;
-        displayvariable(numbernotretirees, notretirees);
 
-    }
 
 }
 function ClientInfo(b){
-    myElement6.innerHTML = "Your information";
+    myElement6.innerHTML = "Today's qualification";
     myElement7.innerHTML = "";
     myElement8.innerHTML = "";
     myElement9.innerHTML = "";
@@ -116,32 +75,16 @@ function ClientInfo(b){
 }
 
 $.getJSON( "information.json", function( data ) {
-    // Displaying the messages
-    displayvariable(namevariable1, data.firstName);
-    displayvariable(namevariable2, data.lastName);  
-    fullName = data.firstName + " " + data.lastName;
-    displayvariable(namevariable3, fullName);
-    nextAge = (future-thisyear) + Number(data.age);
-    displayvariable(namevariable4, thisyear);
-    console.log("Today you are " + data.age + ", but in " + future + " you will be " + nextAge + " years old.");
-
+    PeopleQualified = data.YearQualified + " or older";
     // Writting the person's information on the table
-    firstname.innerHTML = data.firstName;
-    lastname.innerHTML = data.lastName;
-    actualage.innerHTML = data.age;
+    YearsQualified.innerHTML = PeopleQualified;
+    YearToday.innerHTML = data.YearToday;
+    age.innerHTML = data.age;
 
     // Writting the person's result
     
-    if(nextAge >= 60){
-        postiveOutcome = "Congratulations " + fullName + ", you may qualify for retirement in " + future;
-        myElement15.innerHTML = postiveOutcome;
-    
-    }
-    else{
-        negativeOutcome = "I'm sorry, " + fullName + ", you won't qualify for retirement in " + future;
-        myElement15.innerHTML = negativeOutcome;
+    postiveOutcome = "Congratulations! If you were born in " + data.YearQualified + ", you may be now qualified for retirement";
+    myElement15.innerHTML = postiveOutcome;
 
-
-    }
 });
 
